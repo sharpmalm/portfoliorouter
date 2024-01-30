f52cdb9f3aaa23d2062b5006a9fe16bb64a2b7f4
sharpmalm <no@example.com>
2024-01-30 15:35:10 +0100

    Unstyled navbar added and working

--- src/content/Home.js
+++ src/content/Home.js
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
 
 function Home() {
     return ( 
         <>
+        <NavigationBar />
         <h1>Welcome to my home page!</h1>
         <p>
             Go to <Link to="/about">About page</Link>
---
+++ src/content/components/NavigationBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const NavigationBar = () => {
+  return (
+    <div>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/contact">Contact</Link>
+      </li>
+    </div>
+  );
+}
+
+export default NavigationBar;
