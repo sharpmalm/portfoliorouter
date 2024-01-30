6fe3a4d84647e10622e87768f3d541c00ccbdce8
sharpmalm <no@example.com>
2024-01-30 15:07:04 +0100

    NOW, true SPA navigation with Link. No refresh.

--- src/content/Home.js
+++ src/content/Home.js
@@ -1,8 +1,12 @@
+import { Link } from 'react-router-dom';
+
 function Home() {
     return ( 
         <>
         <h1>Welcome to my home page!</h1>
-        <p>Go to <a href="/about">About page</a></p>
+        <p>
+            Go to <Link to="/about">About page</Link>
+        </p>
         </>
         
      );
