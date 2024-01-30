1756690e8571e10f7976aa0fc347a5d3bb0fe5c9
sharpmalm <no@example.com>
2024-01-30 14:42:03 +0100

    Two page routing success! One working link on home page, but need to implement true single page (non-refresh) routing.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './content/Home';
+import About from './content/About';
 
 //Home, About, Projects, Programming, Contact
 
 const router = createBrowserRouter([
   { path: '/', element: <Home /> },
+  { path: '/about', element: <About /> },
 ]);
 
 
---
+++ src/content/About.js
@@ -0,0 +1,7 @@
+function About() {
+    return ( 
+        <p>I am a recently educated C# programmer looking for my first paid assignment!</p>
+     );
+}
+
+export default About;
