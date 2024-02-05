edaf859c460470fc9426a488c962ac1c1098b40c
sharpmalm <no@example.com>
2024-02-05 16:06:11 +0100

    Fixed Projects page with background pic and clickable links to github repos.

--- src/content/Projects.js
+++ src/content/Projects.js
@@ -1,6 +1,22 @@
+import './Projects.css'
 function Projects() {
     return ( 
-        <h1>Here is a list of my most recently completed projects from school with brief descriptions and links to the GitHub repositories.</h1>
+        <div className="Project">
+            <label className='plabel'>Here is a list of my most recently completed projects from</label>
+            <label className='plabel'>school with brief descriptions and links to the GitHub repos</label>
+            <label className="plabel2">
+                <a href="https://github.com/sharpmalm/Cakes" target="_blank" rel="noreferrer">
+                    CAKES
+                </a>
+            </label>
+            <label className="plabel2">
+                <a href="https://github.com/sharpmalm/Asset Tracking" target="_blank" rel="noreferrer">
+                    ASSET TRACKING
+                </a>
+            </label>
+            {/* <h1>Here is a list of my most recently completed projects from school with brief descriptions and links to the GitHub repositories.</h1> */}
+        </div>
+        
      );
 }
 
